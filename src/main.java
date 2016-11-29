b8172cee76ea8ec44ad80391555049fa7841d73a
fkardashov <fkardashov@gmail.com>
2016-11-29 21:57:22 +0300

    Init

---
+++ src/main.java
@@ -0,0 +1,12 @@
+import static java.lang.Thread.sleep;
+
+/**
+ * Created by SBT-Kardashov-FM on 29.11.2016.
+ */
+public class main {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+    }
+}
